5a4caad4ab7d037cb76939b7e52e856f1e70007e
Kristjan Skutta <kristjanskutta@gmail.com>
2026-07-26 04:12:46 +0200

    - Fixed a typo.

--- mediaextensions/interface/mediaextensionspublic.h
+++ mediaextensions/interface/mediaextensionspublic.h
@@ -4,5 +4,5 @@
 #include "iwallpaperenginemediaextensions.h"
 
 #define MEDIA_EXTENSIONS_VERSION "WallpaperEngineMediaExtensions0003"
-#define MEDIA_EXTENSIONS_VERSION_EXPORT extern "C" __declspec(dllexport) const char *WallpaperEngineMedaExtensionVersion = MEDIA_EXTENSIONS_VERSION
+#define MEDIA_EXTENSIONS_VERSION_EXPORT extern "C" __declspec(dllexport) const char *WallpaperEngineMediaExtensionVersion = MEDIA_EXTENSIONS_VERSION
 #define MEDIA_EXTENSIONS_FACTORY extern "C" __declspec(dllexport) IWallpaperEngineMediaExtensions* __cdecl CreateMediaExtensions()
